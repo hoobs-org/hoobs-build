5c8986476f50d515d5f59a31b2563d989a5e154c
oznu <dev@oz.nu>
2019-06-01 05:58:59 +0000

    hoobs ssh motd

--- stage3_custom/02-wifi-connect/01-run.sh
+++ stage3_custom/02-wifi-connect/01-run.sh
@@ -5,6 +5,7 @@
 #
 
 install -m 644 files/issue "${ROOTFS_DIR}/etc/issue"
+install -m 644 files/motd "${ROOTFS_DIR}/etc/update-motd.d/20-hoobs"
 install -m 644 files/wifi-connect.service "${ROOTFS_DIR}/etc/systemd/system/"
 install -m 644 files/wifi-connect-startup "${ROOTFS_DIR}/usr/local/sbin/"
 install -m 644 files/raspbian-install.sh "${ROOTFS_DIR}/"
@@ -17,6 +18,7 @@ sudo /raspbian-install.sh -y
 rm -rf /raspbian-install.sh
 
 chmod +x /usr/local/sbin/wifi-connect-startup
+chmod +x /etc/update-motd.d/20-hoobs
 
 systemctl daemon-reload
 systemctl enable wifi-connect
