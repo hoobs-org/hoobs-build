f9c77a2d122900e56ea0adedbecb871859e313f9
Michael Kellsy <mkellsy@gmail.com>
2019-10-03 18:30:04 -0600

    use node package and n

--- stage3/00-node-install/01-run.sh
+++ stage3/00-node-install/01-run.sh
@@ -14,7 +14,9 @@ echo "Installing Node.js"
 set -e
 set -x
 
-curl -Lsf "https://nodejs.org/dist/latest-v10.x/node-v10.16.3-linux-armv7l.tar.gz" | tar xzf - -C /usr/local --strip-components=1 --no-same-owner
+npm install -g n
+
+n stable
 
 node -v
 npm -v
