a8d40a691433abd137d6197cfb8156664306a267
Michael Kellsy <mkellsy@gmail.com>
2019-10-03 19:26:34 -0600

    mkdir if not exists

--- stage3/03-nginx/01-run.sh
+++ stage3/03-nginx/01-run.sh
@@ -4,6 +4,8 @@
 # Setup nginx
 #
 
+mkdir -p /usr/share/hoobs
+
 install -m 644 files/nginx.conf "${ROOTFS_DIR}/etc/nginx/"
 install -m 644 files/hoobs.conf "${ROOTFS_DIR}/etc/nginx/conf.d/"
 install -m 644 files/loader.html "${ROOTFS_DIR}/usr/share/hoobs/"
