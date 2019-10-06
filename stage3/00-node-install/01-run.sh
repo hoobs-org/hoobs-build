a3f478507c9aa9a0f95bc18c4675e8cd245e8335
Michael Kellsy <mkellsy@gmail.com>
2019-10-05 19:59:06 -0600

    add yarn

--- stage3/00-node-install/01-run.sh
+++ stage3/00-node-install/01-run.sh
@@ -18,6 +18,12 @@ curl -kfLO https://nodejs.org/dist/latest-v10.x/node-v10.16.3-linux-armv7l.tar.g
 tar -xzf node-v10.16.3-linux-armv7l.tar.gz -C /usr/local --strip-components=1 --no-same-owner
 rm -rf node-v10.16.3-linux-armv7l.tar.gz
 
+curl -ksS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+apt-get update && apt-get install -y yarn
+
 node -v
 npm -v
+yarn -v
 EOF
