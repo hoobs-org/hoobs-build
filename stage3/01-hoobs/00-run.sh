9c3d17355c5a269497607be55099ac7a271ff8cf
Michael Kellsy <mkellsy@gmail.com>
2019-10-05 20:06:57 -0600

    install with yarn

--- stage3/01-hoobs/00-run.sh
+++ stage3/01-hoobs/00-run.sh
@@ -4,7 +4,7 @@ install -m 644 files/hoobs.service "${ROOTFS_DIR}/etc/systemd/system/"
 
 on_chroot << EOF
 # install hoobs-core
-npm install -g --unsafe-perm @hoobs/hoobs
+yarn global add --unsafe-perm --ignore-engines @hoobs/hoobs
 
 # grant hoobs user password-less sudo
 echo 'hoobs    ALL=(ALL) NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo
