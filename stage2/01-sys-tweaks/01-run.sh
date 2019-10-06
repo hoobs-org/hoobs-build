7449e81f0d381ddc593b9cafd3d52b837b6ca11d
Michael Kellsy <mkellsy@gmail.com>
2019-10-05 18:35:30 -0600

    resize fs

--- stage2/01-sys-tweaks/01-run.sh
+++ stage2/01-sys-tweaks/01-run.sh
@@ -6,7 +6,6 @@ install -m 644 files/ttyoutput.conf "${ROOTFS_DIR}/etc/systemd/system/rc-local.s
 install -m 644 files/50raspi "${ROOTFS_DIR}/etc/apt/apt.conf.d/"
 install -m 644 files/console-setup "${ROOTFS_DIR}/etc/default/"
 install -m 755 files/rc.local "${ROOTFS_DIR}/etc/"
-install -m 755 files/init-fs "${ROOTFS_DIR}/etc/"
 
 on_chroot << EOF
 systemctl disable hwclock.sh
