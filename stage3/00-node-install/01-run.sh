914b8d72633b2c15e3af553f219d5630fadb0c3e
Michael Kellsy <mkellsy@gmail.com>
2019-10-23 12:12:48 -0600

    update node

--- stage3/00-node-install/01-run.sh
+++ stage3/00-node-install/01-run.sh
@@ -9,8 +9,8 @@ set -e
 set -x
 
 curl -kfLO https://nodejs.org/dist/latest-v10.x/node-v10.17.0-linux-armv7l.tar.gz
-tar -xzf node-v10.16.3-linux-armv7l.tar.gz -C /usr/local --strip-components=1 --no-same-owner
-rm -rf node-v10.16.3-linux-armv7l.tar.gz
+tar -xzf node-v10.17.0-linux-armv7l.tar.gz -C /usr/local --strip-components=1 --no-same-owner
+rm -rf node-v10.17.0-linux-armv7l.tar.gz
 
 curl -ksS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
