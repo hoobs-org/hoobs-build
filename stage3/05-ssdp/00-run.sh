a5ac66f6fa434f1e38da71f425dfb21e429f58cd
Michael Kellsy <mkellsy@gmail.com>
2019-10-24 09:21:27 -0600

    stop downloading ssdpd

--- stage3/05-ssdp/00-run.sh
+++ stage3/05-ssdp/00-run.sh
@@ -5,7 +5,7 @@
 #
 
 install -m 644 files/ssdpd.service "${ROOTFS_DIR}/etc/systemd/system/"
-install -m 644 files/ssdp-responder-v1.6.1-linux-arm.tar.gz "${ROOTFS_DIR}/etc/systemd/system/"
+install -m 644 files/ssdp-responder-v1.6.1-linux-arm.tar.gz "${ROOTFS_DIR}/"
 
 on_chroot << EOF
 tar -xzf /ssdp-responder-v1.6.1-linux-arm.tar.gz -C / --no-same-owner
