725c711bfe1b6b3b75bf4e2270a4c163a790d547
Michael Kellsy <mkellsy@gmail.com>
2019-12-17 08:35:24 -0700

    downgrade Node to 10.17.0

--- stage3/00-node-install/01-run.sh
+++ stage3/00-node-install/01-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e 
 
-install -m 644 files/node-v12.13.1-linux-armv7l.tar.gz "${ROOTFS_DIR}/"
+install -m 644 files/node-v10.17.0-linux-armv7l.tar.gz "${ROOTFS_DIR}/"
 
 on_chroot << EOF
 uname -a
@@ -10,13 +10,13 @@ echo "Installing Node.js"
 set -e
 set -x
 
-tar -xzf /node-v12.13.1-linux-armv7l.tar.gz -C /usr/local --strip-components=1 --no-same-owner
-rm -rf /node-v12.13.1-linux-armv7l.tar.gz
+tar -xzf /node-v10.17.0-linux-armv7l.tar.gz -C /usr/local --strip-components=1 --no-same-owner
+rm -rf /node-v10.17.0-linux-armv7l.tar.gz
 
 curl -ksS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-npm install -g npm
+npm install -g npm@6.11.3
 apt-get update && apt-get install -y yarn
 
 node -v
