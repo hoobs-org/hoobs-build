f35c0fe098f2c4d11484e7b91f334147a5194bd8
Michael Kellsy <mkellsy@gmail.com>
2020-01-02 17:46:09 -0700

    prepare for 3.1.17

--- stage3/01-hoobs/00-run.sh
+++ stage3/01-hoobs/00-run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e 
 
 install -m 644 files/hoobs.service "${ROOTFS_DIR}/etc/systemd/system/"
-install -m 644 files/hoobs-test-reset "${ROOTFS_DIR}/usr/bin/"
 install -m 644 files/hoobs-core.tgz "${ROOTFS_DIR}/"
 
 on_chroot << EOF
@@ -14,6 +13,4 @@ echo 'hoobs    ALL=(ALL) NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo
 
 systemctl daemon-reload
 systemctl enable hoobs
-
-chmod 755 /usr/bin/hoobs-test-reset
 EOF
