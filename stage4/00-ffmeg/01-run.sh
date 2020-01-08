4f1cdb6b595c7b505f76c97c1963b6c26fef3889
Michael Kellsy <mkellsy@gmail.com>
2020-01-08 10:15:24 -0700

    remove cross compile since arch is set

--- stage4/00-ffmeg/01-run.sh
+++ stage4/00-ffmeg/01-run.sh
@@ -9,7 +9,7 @@ git clone https://github.com/hoobs-org/fdk-aac.git
 cd ./fdk-aac
 
 ./autogen.sh
-./configure --prefix=/usr/local --enable-shared --enable-static --enable-cross-compile
+./configure --prefix=/usr/local --enable-shared --enable-static
 
 make -j4
 
@@ -23,7 +23,7 @@ rm -fR ./ffmpeg
 git clone https://github.com/hoobs-org/ffmpeg.git
 cd ./ffmpeg
 
-./configure --prefix=/usr/local --arch=armel --target-os=linux --enable-omx-rpi --enable-nonfree --enable-gpl --enable-libfdk-aac --enable-mmal --enable-libx264 --enable-decoder=h264 --enable-network --enable-protocol=tcp --enable-demuxer=rtsp --enable-cross-compile
+./configure --prefix=/usr/local --arch=armel --target-os=linux --enable-omx-rpi --enable-nonfree --enable-gpl --enable-libfdk-aac --enable-mmal --enable-libx264 --enable-decoder=h264 --enable-network --enable-protocol=tcp --enable-demuxer=rtsp
 make -j4
 
 make install
