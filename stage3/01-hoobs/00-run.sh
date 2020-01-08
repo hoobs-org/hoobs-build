59de801a18a9b443d21bda3874e755cb7cf8eba1
Michael Kellsy <mkellsy@gmail.com>
2020-01-08 09:25:19 -0700

    add ffmpeg for armv7l

--- stage3/01-hoobs/00-run.sh
+++ stage3/01-hoobs/00-run.sh
@@ -4,11 +4,9 @@ install -m 644 files/hoobs.service "${ROOTFS_DIR}/etc/systemd/system/"
 install -m 644 files/hoobs-core.tgz "${ROOTFS_DIR}/"
 
 on_chroot << EOF
-# install hoobs-core
 npm install -g --unsafe-perm /hoobs-core.tgz
 rm -rf /hoobs-core.tgz
 
-# grant hoobs user password-less sudo
 echo 'hoobs    ALL=(ALL) NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo
 
 systemctl daemon-reload
---
+++ stage4/00-ffmeg/01-run.sh
@@ -0,0 +1,28 @@
+on_chroot << EOF
+set -x
+
+git clone https://github.com/hoobs-org/fdk-aac.git
+cd ./fdk-aac
+
+./autogen.sh
+./configure --prefix=/usr/local --enable-shared --enable-static
+
+make -j4
+
+make install
+ldconfig
+
+cd ../
+rm -fR ./fdk-aac
+
+git clone https://github.com/hoobs-org/ffmpeg.git
+cd ./ffmpeg
+
+./configure --prefix=/usr/local --arch=armel --target-os=linux --enable-omx-rpi --enable-nonfree --enable-gpl --enable-libfdk-aac --enable-mmal --enable-libx264 --enable-decoder=h264 --enable-network --enable-protocol=tcp --enable-demuxer=rtsp
+make -j4
+
+make install
+
+cd ../
+rm -fR ./ffmpeg
+EOF
