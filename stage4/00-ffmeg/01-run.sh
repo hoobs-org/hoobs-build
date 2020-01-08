ecc09ba71f48765b5a29d02cc015fb278988baf7
Michael Kellsy <mkellsy@gmail.com>
2020-01-08 10:00:33 -0700

    enable cross compile

--- stage4/00-ffmeg/01-run.sh
+++ stage4/00-ffmeg/01-run.sh
@@ -5,7 +5,7 @@ git clone https://github.com/hoobs-org/fdk-aac.git
 cd ./fdk-aac
 
 ./autogen.sh
-./configure --prefix=/usr/local --enable-shared --enable-static
+./configure --prefix=/usr/local --enable-shared --enable-static --enable-cross-compile
 
 make -j4
 
@@ -18,7 +18,7 @@ rm -fR ./fdk-aac
 git clone https://github.com/hoobs-org/ffmpeg.git
 cd ./ffmpeg
 
-./configure --prefix=/usr/local --arch=armel --target-os=linux --enable-omx-rpi --enable-nonfree --enable-gpl --enable-libfdk-aac --enable-mmal --enable-libx264 --enable-decoder=h264 --enable-network --enable-protocol=tcp --enable-demuxer=rtsp
+./configure --prefix=/usr/local --arch=armel --target-os=linux --enable-omx-rpi --enable-nonfree --enable-gpl --enable-libfdk-aac --enable-mmal --enable-libx264 --enable-decoder=h264 --enable-network --enable-protocol=tcp --enable-demuxer=rtsp --enable-cross-compile
 make -j4
 
 make install
