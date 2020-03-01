9873d553217259b3089bdc07e6ee672b99a6fb42
Michael Kellsy <mkellsy@gmail.com>
2020-03-01 13:49:06 -0700

    ffmpeg prereqs

--- stage4/00-ffmeg/00-run.sh
+++ stage4/00-ffmeg/00-run.sh
@@ -8,7 +8,7 @@ uname -a
 set -e
 set -x
 
-apt-get update && apt-get install -y libtool libx264-dev
+apt-get update && apt-get install -y libtool-bin libtool openssl libopus-dev libx264-dev libvpx-dev libvorbis-dev libtheora-dev libmp3lame-dev libfreetype6-dev libass-dev libspeex-dev libfontconfig-dev frei0r-plugins-dev libfribidi-dev librubberband-dev libsoxr-dev libvidstab-dev libwebp-dev libxml2-dev libxvidcore-dev
 
 tar -xzf /ffmpeg.tar.gz -C /usr/local --strip-components=1 --no-same-owner
 rm -rf /ffmpeg.tar.gz
