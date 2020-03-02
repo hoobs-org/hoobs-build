614e9fd1929923a38d7fe31f6ccb688dbe0e6660
Michael Kellsy <mkellsy@gmail.com>
2020-03-01 18:56:10 -0700

    add firefox ca certificages

--- stage3/05-ssdp/00-run.sh
+++ stage3/05-ssdp/00-run.sh
@@ -8,6 +8,8 @@ install -m 644 files/ssdpd.service "${ROOTFS_DIR}/etc/systemd/system/"
 install -m 644 files/ssdp-responder-v1.6.1-linux-arm.tar.gz "${ROOTFS_DIR}/"
 
 on_chroot << EOF
+wget -O /etc/ssl/certs/cacert.pem https://curl.haxx.se/ca/cacert.pem
+
 tar -xzf /ssdp-responder-v1.6.1-linux-arm.tar.gz -C / --no-same-owner
 rm -rf /ssdp-responder-v1.6.1-linux-arm.tar.gz
 
