50f73dc1948aa4da5b404e5e24a60060ef7fccb9
Michael Kellsy <mkellsy@gmail.com>
2020-03-01 20:08:31 -0700

    add cert file to env

--- stage3/05-ssdp/00-run.sh
+++ stage3/05-ssdp/00-run.sh
@@ -10,6 +10,8 @@ install -m 644 files/ssdp-responder-v1.6.1-linux-arm.tar.gz "${ROOTFS_DIR}/"
 on_chroot << EOF
 wget -O /etc/ssl/certs/cacert.pem https://curl.haxx.se/ca/cacert.pem
 
+echo 'SSL_CERT_FILE=/etc/ssl/certs/cacert.pem' | sudo tee -a /etc/environment
+
 tar -xzf /ssdp-responder-v1.6.1-linux-arm.tar.gz -C / --no-same-owner
 rm -rf /ssdp-responder-v1.6.1-linux-arm.tar.gz
 
