243c59a04a4da88ebdfe14eef93035f51c80d278
Michael Kellsy <mkellsy@gmail.com>
2020-04-13 15:10:18 -0600

    update wifi connect

--- stage8/00-hoobs/01-run.sh
+++ stage8/00-hoobs/01-run.sh
@@ -7,7 +7,7 @@ install -m 644 files/hoobs-server.tgz "${ROOTFS_DIR}/"
 
 on_chroot << EOF
 npm_config_user=root npm install -g --unsafe-perm /hoobs-server.tgz
-npm_config_user=root npm install -g node-pty
+npm_config_user=root npm install -g --unsafe-perm node-pty
 
 rm -rf /hoobs-core.tgz
 
