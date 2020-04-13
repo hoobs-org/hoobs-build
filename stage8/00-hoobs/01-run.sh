82e6b10c390f0b282f2d31abae6af3cacba63050
Michael Kellsy <mkellsy@gmail.com>
2020-04-13 15:01:35 -0600

    set global user to root

--- stage8/00-hoobs/01-run.sh
+++ stage8/00-hoobs/01-run.sh
@@ -6,8 +6,8 @@ install -m 644 files/default.hoobsd.service "${ROOTFS_DIR}/etc/systemd/system/"
 install -m 644 files/hoobs-server.tgz "${ROOTFS_DIR}/"
 
 on_chroot << EOF
-npm install -g --unsafe-perm /hoobs-server.tgz
-npm install -g node-pty
+npm_config_user=root npm install -g --unsafe-perm /hoobs-server.tgz
+npm_config_user=root npm install -g node-pty
 
 rm -rf /hoobs-core.tgz
 
