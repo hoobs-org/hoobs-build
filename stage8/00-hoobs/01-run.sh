91c59518953e9fb2ad2509c40d110a3087c33b13
Michael Kellsy <mkellsy@gmail.com>
2020-04-13 12:39:43 -0600

    upgrade to new code

--- stage8/00-hoobs/01-run.sh
+++ stage8/00-hoobs/01-run.sh
@@ -1,15 +1,13 @@
 #!/bin/bash -e 
 
 install -m 644 files/sudoers "${ROOTFS_DIR}/etc/"
-install -m 644 files/hoobs.service "${ROOTFS_DIR}/etc/systemd/system/"
-install -m 644 files/hoobs-core.tgz "${ROOTFS_DIR}/"
+install -m 644 files/hoobs-server.tgz "${ROOTFS_DIR}/"
 
 on_chroot << EOF
-npm uninstall -g @hoobs/hoobs
-npm install -g --unsafe-perm /hoobs-core.tgz
+npm install -g --unsafe-perm /hoobs-server.tgz
 
 rm -rf /hoobs-core.tgz
 
-systemctl daemon-reload
-systemctl enable hoobs
+hoobs enable cockpit
+hoobs instance create -i Default -s 51825 -b 51826
 EOF
