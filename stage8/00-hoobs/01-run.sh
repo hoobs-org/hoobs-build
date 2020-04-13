8c42cb0f240eba802de064d05527a8955bcc33cd
Michael Kellsy <mkellsy@gmail.com>
2020-04-13 13:24:55 -0600

    can not elevate permissions in build

--- stage8/00-hoobs/01-run.sh
+++ stage8/00-hoobs/01-run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -e 
 
 install -m 644 files/sudoers "${ROOTFS_DIR}/etc/"
+install -m 644 files/console.hoobsd.service "${ROOTFS_DIR}/etc/systemd/system/"
+install -m 644 files/default.hoobsd.service "${ROOTFS_DIR}/etc/systemd/system/"
 install -m 644 files/hoobs-server.tgz "${ROOTFS_DIR}/"
 
 on_chroot << EOF
 npm install -g --unsafe-perm /hoobs-server.tgz
+npm install -g node-pty
 
 rm -rf /hoobs-core.tgz
 
-hoobs enable cockpit
-hoobs instance create -i Default -s 51825 -b 51826
+systemctl enable console.hoobsd.service
+systemctl enable default.hoobsd.service
 EOF
