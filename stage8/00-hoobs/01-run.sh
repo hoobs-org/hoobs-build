21a0191b3f061352a117087163ebf9705256ef7a
Michael Kellsy <mkellsy@gmail.com>
2020-04-13 17:09:39 -0600

    use the system to create instances

--- stage8/00-hoobs/01-run.sh
+++ stage8/00-hoobs/01-run.sh
@@ -1,16 +1,13 @@
 #!/bin/bash -e 
 
 install -m 644 files/sudoers "${ROOTFS_DIR}/etc/"
-install -m 644 files/console.hoobsd.service "${ROOTFS_DIR}/etc/systemd/system/"
-install -m 644 files/default.hoobsd.service "${ROOTFS_DIR}/etc/systemd/system/"
 install -m 644 files/hoobs-server.tgz "${ROOTFS_DIR}/"
 
 on_chroot << EOF
 npm_config_user=root npm install -g --unsafe-perm /hoobs-server.tgz
-npm_config_user=root npm install -g --unsafe-perm node-pty
 
 rm -rf /hoobs-core.tgz
 
-systemctl enable console.hoobsd.service
-systemctl enable default.hoobsd.service
+hoobs enable cockpit
+hoobs instance create -i Default -s 51825 -b 51826
 EOF
