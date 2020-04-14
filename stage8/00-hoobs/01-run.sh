3930a69557427615908ee13194accdead08d73a8
Michael Kellsy <mkellsy@gmail.com>
2020-04-13 20:11:34 -0600

    no need to remove cached instance

--- stage8/00-hoobs/01-run.sh
+++ stage8/00-hoobs/01-run.sh
@@ -9,8 +9,5 @@ npm_config_user=root npm install -g --unsafe-perm /hoobs-server.tgz
 rm -rf /hoobs-core.tgz
 
 hoobs enable cockpit
-
-hoobs instance remove -i default
-hoobs instance remove -i console
 hoobs instance create -i Default -s 51825 -b 51826
 EOF
